490b812754a357fe3cff095ae852209a39b1bc66
Remberto J. Cuadro Alvear <rembecuadro@hotmail.com>
2023-10-02 22:00:49 -0500

    Se creó una nueva clase

---
+++ JenkinsLab/src/jenkinslab/JenkinsLab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jenkinslab;
+
+/**
+ *
+ * @author rembe
+ */
+public class JenkinsLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("A commit has been made.");
+    }
+    
+}
---
+++ JenkinsLab/src/jenkinslab/TestJenkins.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package jenkinslab;
+
+//@author rembe
+ 
+public class TestJenkins {
+    
+    int num = 1;
+    String str= "Hola mundo de Jenkins y Git";
+    boolean bool = true;
+    
+
+}
